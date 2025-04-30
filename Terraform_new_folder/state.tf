c19ce35f7c88cffbd86ba5a8f22c8d21c4b06d7a
iac23 <dehancloud77@gmail.com>
2025-04-30 18:29:13 -0400

    created a VPC with subnets, IGW and Route Tables with associations for the subnets using Terraform

--- Terraform_new_folder/state.tf
+++ Terraform_new_folder/state.tf
@@ -3,7 +3,7 @@ terraform {
         bucket = "db-terraform-state1"
         key = "global/s3/terraform.tfstate"
         region = "us-east-1"
-        dynamodb_table = "s3-tf-table"
+        dynamodb_table = "terraform-lock-file"
 
     }
 }
